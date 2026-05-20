2c339711aca9964b1b76d133e0156c19d2391c87
AmitCode <amitkumar.pandey052001@gmail.com>
2026-05-20 19:46:47 +0530

    Completed the email and token based verification and changed the user creation flow

--- src/main/java/com/notifications/template/RegistrationEmailTemplate.java
+++ src/main/java/com/notifications/template/RegistrationEmailTemplate.java
@@ -3,6 +3,6 @@ package com.notifications.template;
 public class RegistrationEmailTemplate {
     public static String generateRegistrationEmailTemplate(String userName, String userEmail, String verifyLinkUrl){
         return String.format("Click below link to verify and complete your user account registration." +
-                "<a href=\"%s\">Click Here!.</a>", verifyLinkUrl);
+                "<a href=\"%s\">Click Here!.</a> ", verifyLinkUrl);
     }
 }
